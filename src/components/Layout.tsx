fe01822948ad9bca9d796285fd741d43f136fb36
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-11-02 20:54:51 +0700

    fix: mobile height

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,14 @@
 import type { JSX } from "solid-js/jsx-runtime";
 
 export function Layout(props: { children: JSX.Element }) {
+  const isMobile = document.body.clientWidth < 640;
+
   return (
     <div class="max-w-4xl mx-auto overflow-auto px-4">
       <div
         class="flex flex-col gap-8"
         style={{
-          "max-height": "calc(100vh - 4em)",
+          "max-height": `calc(100vh - ${isMobile ? "2em" : "4em"})`,
         }}
       >
         {props.children}
