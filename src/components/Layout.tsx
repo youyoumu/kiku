4c988e02048429e8f00a86af1185c10db51a265a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-11-06 21:57:44 +0700

    fix: mobile layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import type { JSX } from "solid-js";
 
 export function Layout(props: { children: JSX.Element }) {
   return (
-    <div class="max-w-4xl mx-auto overflow-auto p-2 sm:p-4 gutter-stable h-svh">
+    <div class="max-w-4xl mx-auto overflow-auto p-2 sm:p-4 gutter-stable h-[calc(100vh-1rem)] sm:h-vh">
       <div class="flex flex-col gap-6 ">{props.children}</div>
     </div>
   );
