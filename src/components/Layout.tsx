200a8aef27665c5cc32e514ff8e30c9426a7d2b3
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-11-06 17:28:31 +0700

    fix: calc height without js

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,9 @@
 import type { JSX } from "solid-js";
-import { useBreakpoint } from "./shared/Context";
 
 export function Layout(props: { children: JSX.Element }) {
-  const bp = useBreakpoint();
   return (
     <div class="max-w-4xl mx-auto overflow-auto px-2 sm:px-4 gutter-stable">
-      <div
-        class="flex flex-col gap-6"
-        style={{
-          height: `calc(100vh - ${bp.isAtLeast("sm") ? "4em" : "2em"})`,
-        }}
-      >
+      <div class="flex flex-col gap-6 h-[calc(100vh-2em)] sm:h-[calc(100vh-4em)]">
         {props.children}
       </div>
     </div>
