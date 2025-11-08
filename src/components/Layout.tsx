a609dec74f43e528582d1153cc1b480007555591
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-11-08 20:52:08 +0700

    feat: configure tailwind primary secondary font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,10 @@ import type { JSX } from "solid-js";
 
 export function Layout(props: { children: JSX.Element }) {
   return (
-    <div class="max-w-4xl mx-auto overflow-auto p-2 sm:p-4 gutter-stable h-[calc(100vh-1rem)] sm:h-vh">
+    <div
+      data-font-primary="true"
+      class="max-w-4xl mx-auto overflow-auto p-2 sm:p-4 gutter-stable h-[calc(100vh-1rem)] sm:h-vh"
+    >
       <div class="flex flex-col gap-6 ">{props.children}</div>
     </div>
   );
