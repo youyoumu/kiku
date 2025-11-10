ffdeae7508dfadce87932f098ab6b73868d11e10
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-11-10 16:21:05 +0700

    fix: no layout on nested

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,10 @@
 import type { JSX } from "solid-js";
+import { useCardStore } from "./shared/Context";
 
 export function Layout(props: { children: JSX.Element }) {
+  const [card] = useCardStore();
+  if (card.nested) return props.children;
+
   return (
     <div class="max-w-4xl mx-auto overflow-auto p-2 sm:p-4 gutter-stable h-[calc(100vh-1rem)] sm:h-vh font-primary">
       <div class="flex flex-col gap-6 ">{props.children}</div>
