f17ed545ff73f58eae3ac2235b556f168e47111d
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-11-12 20:47:36 +0700

    fix: sm height

--- packages/note/src/components/Layout.tsx
+++ packages/note/src/components/Layout.tsx
@@ -6,7 +6,7 @@ export function Layout(props: { children: JSX.Element }) {
   if (card.nested) return props.children;
 
   return (
-    <div class="max-w-4xl mx-auto overflow-auto p-2 sm:p-4 gutter-stable h-[calc(100vh-1rem)] sm:h-vh font-primary">
+    <div class="max-w-4xl mx-auto overflow-auto p-2 sm:p-4 gutter-stable h-[calc(100vh-1rem)] sm:h-svh font-primary">
       <div class="flex flex-col gap-6 ">{props.children}</div>
     </div>
   );
