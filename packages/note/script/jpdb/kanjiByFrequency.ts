fea12fc329e47bc2d9ed0adcf19805886e5cab83
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-12-02 13:43:57 +0700

    build: kanji write html

--- packages/note/script/jpdb/kanjiByFrequency.ts
+++ packages/note/script/jpdb/kanjiByFrequency.ts
@@ -2,6 +2,33 @@ import { mkdir, readFile, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 import * as cheerio from "cheerio";
 
+type Kanji = {
+  svg: string;
+  keyword: string;
+  frequency: string;
+  type: string;
+  kanji: string;
+  heisig: string;
+  readings: {
+    reading: string;
+    percentage: string;
+  }[];
+  composedOf: {
+    kanji: string;
+    keyword: string;
+  }[];
+  usedInKanji: {
+    kanji: string;
+    keyword: string;
+  }[];
+};
+
+type KanjiFreqKind = {
+  position: string;
+  kanji: string;
+  kind: string;
+};
+
 class KanjiByFrequency {
   KYOIKU_URL = "https://jpdb.io/kanji-by-frequency?show_only=kyouiku";
   JOYO_URL = "https://jpdb.io/kanji-by-frequency?show_only=jouyou";
@@ -10,18 +37,22 @@ class KanjiByFrequency {
 
   root = join(import.meta.dirname, "../../");
   jpdbDir = join(this.root, ".jpdb");
-  kanjiByFrequency = join(this.jpdbDir, "kanji-by-frequency");
-  kyoikuHtml = join(this.kanjiByFrequency, "kyoiku.html");
-  kyoikuJson = join(this.kanjiByFrequency, "kyoiku.json");
-  joyoHtml = join(this.kanjiByFrequency, "joyo.html");
-  joyoJson = join(this.kanjiByFrequency, "joyo.json");
-  jinmeiyoHtml = join(this.kanjiByFrequency, "jinmeiyo.html");
-  jinmeiyoJson = join(this.kanjiByFrequency, "jinmeiyo.json");
-  hyogaiHtml = join(this.kanjiByFrequency, "hyogai.html");
-  hyogaiJson = join(this.kanjiByFrequency, "hyogai.json");
+  kanjiByFrequencyDir = join(this.jpdbDir, "kanji-by-frequency");
+  kanjiDir = join(this.jpdbDir, "kanji");
+  kanjiErrorJson = join(this.jpdbDir, "kanji-error.json");
+
+  kyoikuHtml = join(this.kanjiByFrequencyDir, "kyoiku.html");
+  kyoikuJson = join(this.kanjiByFrequencyDir, "kyoiku.json");
+  joyoHtml = join(this.kanjiByFrequencyDir, "joyo.html");
+  joyoJson = join(this.kanjiByFrequencyDir, "joyo.json");
+  jinmeiyoHtml = join(this.kanjiByFrequencyDir, "jinmeiyo.html");
+  jinmeiyoJson = join(this.kanjiByFrequencyDir, "jinmeiyo.json");
+  hyogaiHtml = join(this.kanjiByFrequencyDir, "hyogai.html");
+  hyogaiJson = join(this.kanjiByFrequencyDir, "hyogai.json");
 
   async mkdir() {
-    await mkdir(this.kanjiByFrequency, { recursive: true });
+    await mkdir(this.kanjiByFrequencyDir, { recursive: true });
+    await mkdir(this.kanjiDir, { recursive: true });
   }
 
   async writeKanjiByFrequencyHtml() {
@@ -81,10 +112,70 @@ class KanjiByFrequency {
       await writeFile(dest, JSON.stringify(json, null, 2));
     }
   }
+
+  async writeKanjiHtml(someKanjis?: string[]) {
+    const kyoiku = JSON.parse(
+      await readFile(this.kyoikuJson, "utf8"),
+    ) as KanjiFreqKind[];
+    const joyo = JSON.parse(
+      await readFile(this.joyoJson, "utf8"),
+    ) as KanjiFreqKind[];
+    const jinmeiyo = JSON.parse(
+      await readFile(this.jinmeiyoJson, "utf8"),
+    ) as KanjiFreqKind[];
+    const hyogai = JSON.parse(
+      await readFile(this.hyogaiJson, "utf8"),
+    ) as KanjiFreqKind[];
+
+    const merged = [...kyoiku, ...joyo, ...jinmeiyo, ...hyogai];
+    const kanjis = someKanjis ?? [...new Set(merged.map((k) => k.kanji))];
+
+    console.log(`Total kanji to fetch: ${kanjis.length}`);
+    const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+    let kanjiError: string[] = [];
+
+    for (let i = 0; i < kanjis.length; i++) {
+      const kanji = kanjis[i];
+      const url = `https://jpdb.io/kanji/${encodeURIComponent(kanji)}?expand=k`;
+
+      console.log(`[${i + 1}/${kanjis.length}] Fetching ${kanji} (${url})`);
+
+      try {
+        const res = await fetch(url);
+        const html = await res.text();
+
+        const dest = join(this.kanjiDir, `${kanji}.html`);
+        await writeFile(dest, html);
+
+        console.log(`Saved → ${dest}`);
+      } catch (err) {
+        kanjiError.push(kanji);
+        console.error(`Failed to fetch ${kanji}:`, err);
+      }
+
+      await sleep(250);
+    }
+
+    try {
+      const kanjiErrorJson = JSON.parse(
+        await readFile(this.kanjiErrorJson, "utf8"),
+      );
+      kanjiError = Array.from(new Set([...kanjiError, ...kanjiErrorJson]));
+    } catch {
+      console.log("Error reading kanjiErrorJson");
+    }
+    await writeFile(this.kanjiErrorJson, JSON.stringify(kanjiError, null, 2));
+  }
 }
 
 const kanjiByFrequency = new KanjiByFrequency();
-// await kanjiByFrequency.mkdir();
+await kanjiByFrequency.mkdir();
+
+// fist step
 // kanjiByFrequency.writeKanjiByFrequencyHtml();
 
-await kanjiByFrequency.writeKanjiByFrequencyJson();
+// second step
+// await kanjiByFrequency.writeKanjiByFrequencyJson();
+
+// third step
+await kanjiByFrequency.writeKanjiHtml();
