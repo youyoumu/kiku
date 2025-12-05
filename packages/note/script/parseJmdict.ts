39564fb5217155c353346bafba61b42e2e2bc9d0
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-12-05 14:06:13 +0700

    build: parse jmdict termmap

--- packages/note/script/parseJmdict.ts
+++ packages/note/script/parseJmdict.ts
@@ -2,11 +2,18 @@ import { mkdir, readFile, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 import * as cheerio from "cheerio";
 
+type JmdictTerm = {
+  kanji: string[];
+  reading: string[];
+  meanings: string[];
+};
+
 export class JmdictParser {
   ROOT_PATH = join(import.meta.dirname, "../");
   JMDICT_DIR = join(this.ROOT_PATH, ".jmdict");
   JMDICT_PATH = join(this.JMDICT_DIR, "JMdict_e");
   JMDICT_TERM_PATH = join(this.JMDICT_DIR, "term.json");
+  JMDICT_TERM_MAP_PATH = join(this.JMDICT_DIR, "termMap.json");
 
   async ensureDir() {
     await mkdir(this.JMDICT_DIR, { recursive: true });
@@ -55,8 +62,32 @@ export class JmdictParser {
     const terms = await this.parseAll();
     await writeFile(this.JMDICT_TERM_PATH, JSON.stringify(terms, null, 2));
   }
+
+  async writeTermMap() {
+    const terms = JSON.parse(
+      await readFile(this.JMDICT_TERM_PATH, "utf8"),
+    ) as JmdictTerm[];
+    const termMap: Record<string, JmdictTerm> = {};
+    const duplicatedTerms: JmdictTerm[] = [];
+    terms.forEach((term) => {
+      term.kanji.forEach((kanji) => {
+        if (termMap[kanji]) {
+          duplicatedTerms.push(term);
+        } else {
+          termMap[kanji] = term;
+        }
+      });
+    });
+
+    await writeFile(
+      this.JMDICT_TERM_MAP_PATH,
+      JSON.stringify(termMap, null, 2),
+    );
+  }
 }
 
 const jmdictParser = new JmdictParser();
 
-await jmdictParser.writeTerm();
+// await jmdictParser.writeTerm();
+
+await jmdictParser.writeTermMap();
