33be1ee1a200ecd278d44d953347e5860a2948fb
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-12-20 13:47:18 +0700

    feat: only show merge button if ankiConnect

--- packages/note/src/components/_kiku_lazy/MergeContextModal.tsx
+++ packages/note/src/components/_kiku_lazy/MergeContextModal.tsx
@@ -1,32 +1,60 @@
-import { onMount } from "solid-js";
+import { Match, onMount, Switch } from "solid-js";
+import { useBreakpointContext } from "../shared/BreakpointContext";
 import {
   useFieldGroupContext,
   useRootFieldGroupContext,
 } from "../shared/FieldGroupContext";
-import { GitPullRequestArrow } from "./Icons";
+import { useGeneralContext } from "../shared/GeneralContext";
+import { GitPullRequestArrow, RefreshCwIcon } from "./Icons";
 
 export default function MergeContextModal() {
   let dialogRef: HTMLDialogElement | undefined;
+  const [$general] = useGeneralContext();
   const { $group: $rootGroup, ankiFields: rootAnkiFields } =
     useRootFieldGroupContext();
   const { $group, ankiFields } = useFieldGroupContext();
+  const bp = useBreakpointContext();
 
-  onMount(() => {
+  onMount(async () => {
     if (dialogRef) {
       dialogRef.showModal();
     }
+
+    if (!bp.isAtLeast("sm")) return;
+    await $general.checkAnkiConnect();
   });
 
   return (
     <>
-      <GitPullRequestArrow
-        class="size-5 cursor-pointer text-base-content-soft"
-        on:click={() => {
-          if (dialogRef) {
-            dialogRef.showModal();
-          }
-        }}
-      />
+      <Switch>
+        <Match when={$general.isAnkiConnectAvailable}>
+          <GitPullRequestArrow
+            class="size-4 cursor-pointer text-base-content-soft"
+            on:click={() => {
+              if (dialogRef) {
+                dialogRef.showModal();
+              }
+            }}
+          />
+        </Match>
+        <Match when={!$general.isAnkiConnectAvailable}>
+          <div class="indicator">
+            <div class="place-items-center">
+              <RefreshCwIcon
+                class="size-4 cursor-pointer text-base-content-soft"
+                on:click={async () => {
+                  try {
+                    await $general.checkAnkiConnect();
+                  } catch {
+                    $general.toast.error("AnkiConnect is not available");
+                  }
+                }}
+              />
+            </div>
+            <span class="status status-error animate-ping"></span>
+          </div>
+        </Match>
+      </Switch>
 
       <dialog class="modal" ref={dialogRef}>
         <div class="modal-box">
